53662e0f007a2c9369bd80143f21184cda88f487
Iarley-01 <yarleyalves2016@gmail.com>
2025-01-06 21:01:06 -0300

    iniciando a implementação da renderização do player

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,19 @@ window.addEventListener("load", function() {
   }
   
   class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = 0;
+    }
     
+    draw(context) {
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y,this.width, this.height);
+    }
   }
   
   class Background {
@@ -53,6 +65,7 @@ window.addEventListener("load", function() {
   }
   
   const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
   
   function animate () {
     
